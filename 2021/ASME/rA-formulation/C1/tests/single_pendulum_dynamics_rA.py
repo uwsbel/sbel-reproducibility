ac1a9c0b3774888b0ecbe777cf0219614b38b041
alexandrakissel <akissel@wisc.edu>
2021-04-08 09:20:53 -0500

    Add no plotting scripts for timing

--- 2021/ASME/rA-formulation/C1/tests/single_pendulum_dynamics_rA.py
+++ 2021/ASME/rA-formulation/C1/tests/single_pendulum_dynamics_rA.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python3
-
-
 import sys
 import pathlib as pl
 src_folder = pl.Path('../src/')
 sys.path.append(str(src_folder))
 
 import numpy as np
-from copy import copy
-import matplotlib.pyplot as plt
-
 from rA_sim_engine_3d import rASimEngine3D
-import rA_gcons as gcons
 
 sys = rASimEngine3D("../models/revJoint.mdl")
 
@@ -27,29 +21,10 @@ for j, body in enumerate(sys.bodies_list[1:2]):
     body.J = np.diag([J_xx, J_yz, J_yz])
     sys.nb += 1
 
-
 sys.t_start = 0
-sys.t_end = 1
+sys.t_end = 3
 sys.h = 1e-3
-
 sys.max_iters = 20
 sys.tol = 1e-3
 
 sys.dynamics_solver()
-
-# print positions of bodies
-link1 = sys.r_ddot_sol[:, 0:3]
-
-# _, ax1 = plt.subplots()
-# ax1.plot(sys.t_grid, link1[:, 0])
-# ax1.set_title('Link 1, x')
-#
-# _, ax2 = plt.subplots()
-# ax2.plot(sys.t_grid, link1[:, 1])
-# ax2.set_title('Link 1, y')
-#
-# _, ax3 = plt.subplots()
-# ax3.plot(sys.t_grid, link1[:, 2])
-# ax3.set_title('Link 1, z')
-#
-# plt.show()
