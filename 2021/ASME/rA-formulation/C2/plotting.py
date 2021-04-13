6ad68daf73fa445cb71902b1f24e110774f13b08
Jay Taves <jht99@cornell.edu>
2021-04-13 07:36:36 -0500

    Update iterations plotting script

--- 2021/ASME/rA-formulation/C2/plotting.py
+++ 2021/ASME/rA-formulation/C2/plotting.py
@@ -1,13 +1,17 @@
+from multiprocessing import Pool
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-from multiprocessing import Pool
+from SimEngineMBD.example_models.single_pendulum import run_single_pendulum
+from SimEngineMBD.example_models.four_link import run_four_link
+from SimEngineMBD.example_models.slider_crank import run_slider_crank
 
-from four_link_oa import four_link
-from slider_crank_oa import slider_crank
-from single_pendulum_oa import single_pendulum
+model_fn = run_four_link
+mode = 'dynamics'
 
-model_fn = four_link
+# run_some_model_name -> Some_Model_Name
+pretty_model_name = ' '.join([word.capitalize() for word in model_fn.__name__[4:].split('_')])
 
 step_sizes = [1e-3, 2e-3, 4e-3, 8e-3, 1e-2, 2e-2]
 tols = [1e-10 / step**2 for step in step_sizes]
@@ -22,7 +26,7 @@ for form in ['rA', 'rp', 'reps']:
     iters = []
     for steps, tol in zip(step_sizes, tols):
 
-        args = ['--form', form, '--mode', 'kinematics', '--tol', str(1e-10), '--step_size', str(steps)]
+        args = ['--form', form, '--mode', mode, '--tol', str(tol), '--step_size', str(steps)]
 
         _, _, _, num_iters, _ = model_fn(args)
 
@@ -31,7 +35,7 @@ for form in ['rA', 'rp', 'reps']:
     pretty_form = 'rε' if form == 'reps' else form
     ax.plot(step_sizes, iters, ticks[form], label=pretty_form, markersize=15, linewidth=7)
 
-ax.set(xlabel='Tolerance', ylabel='Average Iterations to Convergence', title='Iterations to Convergence, Four Link Model, Kinematics')
+ax.set(xlabel='Step Size', ylabel='Average Iterations to Convergence', title='Iterations to Convergence, {} Model, {}'.format(pretty_model_name, mode.capitalize()))
 ax.legend(loc='best', prop={'size': 24})
 ax.set_xscale('log')
 
